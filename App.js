21b1a6a9d24c988b44f1d0b8f1e583f41faeaa4a
Mayanksaininh <mayanksaininh@gmail.com>
2025-06-29 23:00:51 +0530

    new changes

---
+++ App.js
@@ -0,0 +1,8 @@
+
+const heading  = React.createElement("h1" , {id : "haaeding" ,xyz : "some"} , "Hello world from react");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading)
+
+// console.log(heading)
