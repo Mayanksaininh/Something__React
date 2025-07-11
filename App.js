3c8da7ba64ab83f83e08b68c8effc73fd4ae8aee
Mayanksaininh <mayanksaininh@gmail.com>
2025-07-11 23:29:19 +0530

    Adding Cards

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import React from "react"
 
 import ReactDOM from "react-dom/client"
 
+
+
 const Header = () => {
     return (
         <div className="header">
             <div className="Logo-container">
-                <img scr = "https://images-platform.99static.com//Ba6VdSQsbU4OpiyQEzLi7yHy9KQ=/440x521:1494x1575/fit-in/500x500/99designs-contests-attachments/127/127439/attachment_127439993"></img>
+                <img scr = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUGz0WshFYnlwlqP_fs3ErIppUvpuaYfTrZQ&s' alt="logo"/>
             </div>
             <div className="Nav-item"> 
                 <ul>
@@ -21,14 +23,78 @@ const Header = () => {
     )
 }
 
-const Applayout = () => {
+const Restaurant = (props) => {
     return (
-        <div className="app">
-            {Header}
+        <div className="res-card"   style={{backgroundColor: "gray"}}>
+            <img className="res-logo"
+            src = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/cvo1djhbwrgfqd64k0tl" alt = "res-img"/>
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>{props.rating}</h4>
+        </div>
+
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                <h3>Search</h3>
+            </div>
+            <div className="restaurant-container">
+                <Restaurant
+                resName = "Meghna foods"
+                cuisine = "Biryani north Indian"
+                rating = "4.3 rating"/>
+
+                <Restaurant
+                 resName = "KFC"
+                cuisine = "Burger"
+                rating = "4.5 rating"/>
+
+                <Restaurant
+                 resName = "Roti wala"
+                cuisine = "Tawa roti"
+                rating = "4.2 rating"/>
+
+                <Restaurant
+                 resName = "Kisaan"
+                cuisine = "Momos"
+                rating = "4.7 rating"/>
+
+                <Restaurant
+                 resName = "hungries"
+                cuisine = "Pizza"
+                rating = "4.3 rating"/>
+
+                <Restaurant
+                 resName = "Origano"
+                cuisine = "Masala-Dosa"
+                rating = "4.4 rating"/>
+
+                <Restaurant
+                 resName = "Janak Dhaba"
+                cuisine = "kadai paneer"
+                rating = "4.5 rating"/>
+              
+                      
+            </div>
 
         </div>
     )
 }
+
+const Applayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body/>
+        </div>
+    )
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<Applayout/>)
